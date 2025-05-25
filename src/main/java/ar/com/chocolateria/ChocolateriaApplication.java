67e37b6aa478310f83ae8e6d2a995724a099280e
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-05-25 08:22:12 -0300

    primer commit

---
+++ src/main/java/ar/com/chocolateria/ChocolateriaApplication.java
@@ -0,0 +1,13 @@
+package ar.com.chocolateria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolateriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolateriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/chocolateria/ChocolateriaApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.chocolateria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
