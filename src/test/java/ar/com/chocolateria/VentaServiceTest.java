e8f46898e3c4f045ae2e251dd5a349203c34efbf
Portaluppi Giancarlo Alberto <gianca307@gmail.com>
2025-07-10 12:22:17 -0300

    Agrega VentaServiceTest
    Agrega setup

---
+++ src/test/java/ar/com/chocolateria/VentaServiceTest.java
@@ -0,0 +1,47 @@
+package ar.com.chocolateria;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ar.com.chocolateria.domain.Producto;
+import ar.com.chocolateria.domain.ProductoVendido;
+import ar.com.chocolateria.domain.Venta;
+import ar.com.chocolateria.service.VentaService;
+import lombok.RequiredArgsConstructor;
+
+@SpringBootTest
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class VentaServiceTest extends BaseTest{
+	private final VentaService ventaService;
+	private Venta ventaGuardada;
+	
+	@BeforeEach
+	void setup () {
+		Venta venta = new Venta();
+		List<ProductoVendido> productosVendidos = new ArrayList<>();
+		
+		ProductoVendido productoVendido = new ProductoVendido();
+		productoVendido.setCantidad(5);
+		productoVendido.setPrecio(13600f);
+		Producto producto = new Producto();
+		producto.setId(15L);
+		productoVendido.setProducto(producto);
+		
+		ProductoVendido productoVendido1 = new ProductoVendido();
+		productoVendido1.setCantidad(2);
+		productoVendido1.setPrecio(32000f);
+		Producto producto1 = new Producto();
+		producto1.setId(16L);
+		productoVendido1.setProducto(producto1);
+		
+		productosVendidos.add(productoVendido1);
+		productosVendidos.add(productoVendido);
+		
+		ventaGuardada = this.ventaService.guardarVenta(venta);
+	}
+	
+}
